5704c2908b19bb37583dff6b3f5bcfa921cfe84a
jantapabh <jantapa2407@gmail.com>
2022-05-19 15:24:48 +0700

    update

--- index.ts
+++ index.ts
@@ -1,17 +1,18 @@
-import axios from "axios";
-const url1 = 'https://ftx.com/api/markets/BTC/USDT';
-const url2 = 'https://api1.binance.com/api/v3/avgPrice?symbol=BTCUSDT';
-let a: number=0;
-let b: number=0;
+import axios from 'axios'
+const url1 = 'https://ftx.com/api/markets/BTC/USDT'
+const url2 = 'https://api1.binance.com/api/v3/avgPrice?symbol=BTCUSDT'
+let a: number = 0
+let b: number = 0
 
-axios.get(url1).then(response => {
-    console.log("Binance BTC Price:"+ response.data.result.price+"USDT")
-    a=response.data.result.price;
-    axios.get(url2).then(response => {
-        console.log("FTX BTC Price:"+ response.data.price+"USDT")
-        b=response.data.price
-        const sum = a - b;
-        console.log(sum);
-    });
-});
+async function func() {
+    let response = await axios.get(url1)
+    console.log('Binance BTC Price:' + response.data.result.price + 'USDT')
+    a = response.data.result.price
+    response = await axios.get(url2)
+    console.log('FTX BTC Price:' + response.data.price + 'USDT')
+    b = response.data.price
+    const sum = a - b
+    console.log(sum)
+}
 
+func()
