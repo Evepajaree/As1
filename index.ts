ac52b5881232315e52452f045f9efe1587b272fd
jantapabh <jantapa2407@gmail.com>
2022-05-20 14:08:16 +0700

    Jan test

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ const url2 = 'https://api1.binance.com/api/v3/avgPrice?symbol=BTCUSDT'
 let a: number = 0
 let b: number = 0
 
-async function func() {
+const func = async () => {
     let response = await axios.get(url1)
     console.log('Binance BTC Price:' + response.data.result.price + 'USDT')
     a = response.data.result.price
@@ -13,6 +13,9 @@ async function func() {
     b = response.data.price
     const sum = a - b
     console.log(sum)
+    // test
+    console.log("JAN")
+
 }
 
 func()
